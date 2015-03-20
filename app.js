dece277d4aa69fa584fbc891e8e7e6f3de69c2b7
Jon Perritt <jrperritt@gmail.com>
2015-03-20 09:34:13 -0600

    use request and url-join libraries

--- app.js
+++ app.js
@@ -32,8 +32,9 @@ function set_content_service_url() {
 set_mapping_service_url();
 set_content_service_url();
 
-var http = require('http');
-var express = require('express'),
+var request = require('request'),
+    urljoin = require('url-join'),
+    express = require('express'),
     exphbs  = require('express-handlebars');
 
 var app = express();
@@ -42,70 +43,37 @@ app.set('view engine', 'handlebars');
 
 app.get('/', function(req, presenterRes) {
   // form route to query
-  mapping_route = normalize_url(mapping_service_url) + '/at/' + encodeURIComponent(req.hostname) + encodeURIComponent(req.path);
+  mapping_route = urljoin(normalize_url(mapping_service_url), 'at', encodeURIComponent(req.hostname + req.path));
   // query mapping service for content ID
-  http.get(mapping_route, function(mappingRes) {
-    switch(mappingRes.statusCode) {
-      // handle key not found
-      case 404:
-        presenterRes.statusCode = 404;
-        presenterRes.send('Map key not found in mapping service.');
-        break;
-      // handle redirect
-      case 300, 301, 302, 303, 304, 307, 308:
-        presenterRes.redirect(mappingRes.statusCode, mappingRes.headers.location);
-        break;
-      // handle key found
-      case 200:
-        var body = '';
-        mappingRes.on('data', function(chunk) {
-          body += chunk;
-        });
-        mappingRes.on('end', function(){
+  request(mapping_route, function(error, mappingRes, body) {
+    if (!error && mappingRes.statusCode == 200) {
+      body = JSON.parse(body);
+      // form content route
+      content_route = urljoin(normalize_url(content_service_url), 'content', encodeURIComponent(body['content-id']));
+      // query content service for metadata
+      request(content_route, function(error, mappingRes, body){
+        if (!error && mappingRes.statusCode == 200) {
           // parse string reponse to JSON
-          body = JSON.parse(body);
-          // form content route
-          content_route = normalize_url(content_service_url) + '/content/' + encodeURIComponent(body['content-id']);
-          // query content service for metadata
-          http.get(content_route, function(contentRes){
-            switch(contentRes.statusCode) {
-              // handle content ID not found
-              case 404:
-                presenterRes.statusCode = 404;
-                throw new Error('Content not found in content service');
-                break;
-              // handle content ID found
-              case 200:
-                var body = '';
-                contentRes.on('data', function(chunk) {
-                  body += chunk;
-                });
-                contentRes.on('end', function(){
-                  // parse string reponse to JSON
-                  metadata = JSON.parse(body);
-                  // TODO: determine layout structure for content (perhaps calling the
-                  // mapping service or a layout service). For now, it's hard-coded.
-                  layout_type = 'layouts/temp.handlebars';
-                  // render content with metadata
-                  presenterRes.render(layout_type, metadata, function(err, html) {
-                    if(err) {
-                      throw new Error('Error rendering content');
-                    }
-                    // send rendered html to user
-                    presenterRes.send(html);
-                  });
-                });
-                contentRes.on('error', function(e){
-                  throw new Error('Error retrieving data from content service');
-                });
-                break;
+          metadata = JSON.parse(body);
+          // TODO: determine layout structure for content (perhaps calling the
+          // mapping service or a layout service). For now, it's hard-coded.
+          layout_type = 'layouts/temp.handlebars';
+          // render content with metadata
+          presenterRes.render(layout_type, metadata, function(err, html) {
+            if(err) {
+              throw new Error('Error rendering content');
             }
+            // send rendered html to user
+            presenterRes.send(html);
           });
-        });
-        mappingRes.on('error', function(e){
-          throw new Error('Error retrieving data from mapping service');
-        });
-        break;
+        } else {
+          presenterRes.status(404).end();
+          throw new Error('Content not found in content service');
+        }
+      });
+    } else {
+      presenterRes.status(404).end();
+      throw new Error('Map key not found in mapping service.');
     }
   });
 });
