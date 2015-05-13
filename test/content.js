ca6fd96cba0a4df607b277aeee5928702d28be11
Ash Wilson <smashwilson@gmail.com>
2015-05-13 11:46:59 -0400

    Configurable test logging.

--- test/content.js
+++ test/content.js
@@ -6,7 +6,8 @@ settings = {
   MAPPING_SERVICE_URL: "http://mapping",
   CONTENT_SERVICE_URL: "http://content",
   LAYOUT_SERVICE_URL: "http://layout",
-  PRESENTED_URL_DOMAIN: "deconst.horse"
+  PRESENTED_URL_DOMAIN: "deconst.horse",
+  PRESENTER_LOG_LEVEL: process.env.PRESENTER_LOG_LEVEL
 };
 
 config.configure(settings);
