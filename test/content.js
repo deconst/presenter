fd1d42454adc15cd08397d24d0df09e1e22470b8
Ash Wilson <smashwilson@gmail.com>
2015-05-14 15:15:51 -0400

    Test case for interposing domain and protocol.

--- test/content.js
+++ test/content.js
@@ -48,7 +48,7 @@ describe("/*", function () {
       .expect("Rendered the page content with a layout", done);
   });
 
-  it("Collects presented URLs for related content", function (done) {
+  it("collects presented URLs for related content", function (done) {
     var mapping = nock("http://mapping")
       .get("/at/https%3A%2F%2Fdeconst.horse%2Ffoo%2Fbar%2Fbaz")
       .reply(200, { "content-id": "https://github.com/deconst/fake" })
@@ -58,6 +58,43 @@ describe("/*", function () {
       .reply(200, { "presented-url": "https://deconst.horse/two" })
       .get("/url/https%3A%2F%2Fgithub.com%2Fdeconst%2Ffake%2Fthree")
       .reply(200, { "presented-url": "https://deconst.horse/three" });
+
+    var content = nock("http://content")
+      .get("/content/https%3A%2F%2Fgithub.com%2Fdeconst%2Ffake")
+      .reply(200, {
+        assets: [],
+        envelope: { body: "the page content" },
+        results: { sample: [
+            { contentID: "https://github.com/deconst/fake/one" },
+            { contentID: "https://github.com/deconst/fake/two" },
+            { contentID: "https://github.com/deconst/fake/three" }
+        ] }
+      });
+
+    var layout = nock("http://layout")
+      .get("/https%3A%2F%2Fdeconst.horse%2Ffoo%2Fbar%2Fbaz/default")
+      .reply(200, "URLs: {{#each results.sample}}<{{url}}>{{/each}}");
+
+    var rendered = "URLs: <https://deconst.horse/one>" +
+      "<https://deconst.horse/two><https://deconst.horse/three>";
+
+    request(server.create())
+      .get("/foo/bar/baz")
+      .expect(200)
+      .expect("Content-Type", /html/)
+      .expect(rendered, done);
+  });
+
+  it("transforms related content URLs with a presented domain and protocol", function (done) {
+    var mapping = nock("http://mapping")
+      .get("/at/https%3A%2F%2Fdeconst.horse%2Ffoo%2Fbar%2Fbaz")
+      .reply(200, { "content-id": "https://github.com/deconst/fake" })
+      .get("/url/https%3A%2F%2Fgithub.com%2Fdeconst%2Ffake%2Fone")
+      .reply(200, { "presented-url": "http://other.wtf/one" })
+      .get("/url/https%3A%2F%2Fgithub.com%2Fdeconst%2Ffake%2Ftwo")
+      .reply(200, { "presented-url": "http://other.wtf/two" })
+      .get("/url/https%3A%2F%2Fgithub.com%2Fdeconst%2Ffake%2Fthree")
+      .reply(200, { "presented-url": "http://other.wtf/three" });
 
     var content = nock("http://content")
       .get("/content/https%3A%2F%2Fgithub.com%2Fdeconst%2Ffake")
