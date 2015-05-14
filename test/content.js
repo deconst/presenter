142953d064a36c9489d3b7b73f8592dcd5f3b609
Ash Wilson <smashwilson@gmail.com>
2015-05-14 15:11:39 -0400

    Forgot a `var`!

--- test/content.js
+++ test/content.js
@@ -2,10 +2,11 @@
 
 var config = require("../src/config");
 
-settings = {
+var settings = {
   MAPPING_SERVICE_URL: "http://mapping",
   CONTENT_SERVICE_URL: "http://content",
   LAYOUT_SERVICE_URL: "http://layout",
+  PRESENTED_URL_PROTO: "https",
   PRESENTED_URL_DOMAIN: "deconst.horse",
   PRESENTER_LOG_LEVEL: process.env.PRESENTER_LOG_LEVEL
 };
