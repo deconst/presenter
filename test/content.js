fa991bcb0d1407064aa91d257f7b01f40f56ffaa
Ash Wilson <smashwilson@gmail.com>
2015-05-28 07:31:37 -0400

    Unit tests for proxy behavior.

--- test/content.js
+++ test/content.js
@@ -76,17 +76,52 @@ describe("/*", function () {
 
     it("handles proxied content", function (done) {
       var mapping = nock("http://mapping")
-      .get("/at/https%3A%2F%2Fdeconst.horse%2Ffoo%2Fbar%2Fstatic")
-      .reply(200, { "proxy-to": "https://deconst.horse/static" });
+        .get("/at/https%3A%2F%2Fdeconst.horse%2Ffoo%2Fbar%2Fstatic")
+        .reply(200, { "proxy-to": "https://deconst.horse/static" });
 
       var content = nock("https://deconst.horse")
-      .get("/static")
-      .reply(200, "static content");
+        .get("/static")
+        .reply(200, "static content");
 
       request(server.create())
-      .get("/foo/bar/static")
-      .expect(200)
-      .expect("static content", done);
+        .get("/foo/bar/static")
+        .expect(200)
+        .expect("static content", done);
+    });
+
+    it("preserves headers from the upstream service", function (done) {
+      var mapping = nock("http://mapping")
+        .get("/at/https%3A%2F%2Fdeconst.horse%2Ffoo%2Fbar%2Fstatic")
+        .reply(200, { "proxy-to": "https://upstream.horse/service" });
+
+      var content = nock("https://upstream.horse")
+        .get("/service")
+        .reply(200, "static content", {
+          "Content-Type": "text/plain",
+          "X-Some-Header": "neeeeigh"
+        });
+
+      request(server.create())
+        .get("/foo/bar/static")
+        .expect(200)
+        .expect("Content-Type", "text/plain")
+        .expect("X-Some-Header", "neeeeigh")
+        .expect("static content", done);
+    });
+
+    it("preserves response status from the upstream service", function (done) {
+      var mapping = nock("http://mapping")
+        .get("/at/https%3A%2F%2Fdeconst.horse%2Ffoo%2Fbar%2Fstatic")
+        .reply(200, { "proxy-to": "https://upstream.horse/service" });
+
+      var content = nock("https://upstream.horse")
+        .get("/service")
+        .reply(409, "NOPE");
+
+      request(server.create())
+        .get("/foo/bar/static")
+        .expect(409)
+        .expect("NOPE", done);
     });
 
   });
