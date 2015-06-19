06ae32fed76bc29faa8d7812a18ab41b6670e8d8
Keith Bartholomew <keith.bartholomew@rackspace.com>
2015-06-19 14:48:39 -0500

    301 to a trailing slash unless it looks like a file

--- src/server.js
+++ src/server.js
@@ -5,12 +5,25 @@
 var
   express = require('express'),
   logging = require('./logging'),
-  routes = require('./routes');
+  routes = require('./routes'),
+  url = require('url');
 
 exports.create = function () {
   var app = express();
 
   app.use(logging.requestLogger());
+
+  app.use(function (req, res, next) {
+      var trailingSlash = /\/$/;
+      var fileExtension = /.+?\..+?$/;
+      var parsedUrl = url.parse(req.url, true);
+
+      if(trailingSlash.test(parsedUrl.pathname) || fileExtension.test(parsedUrl.pathname)) {
+          return next();
+      }
+
+      res.redirect(301, parsedUrl.pathname + '/' + parsedUrl.search);
+  });
 
   routes.install(app);
 
