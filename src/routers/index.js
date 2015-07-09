984a8fd8cc1663d227adb4f13debb4900c7013f9
Keith Bartholomew <keith.bartholomew@rackspace.com>
2015-07-09 12:45:06 -0500

    defer all response-sending to ResponseHelper to avoid the crash-happy headers sent error

--- src/routers/index.js
+++ src/routers/index.js
@@ -8,13 +8,6 @@ var ResponseHelper = require('../helpers/response');
 var config = require('../config');
 
 exports.install = function (app) {
-    app.use(function (req, res, next) {
-        RequestHelper.request = req;
-        ResponseHelper.response = res;
-
-        next();
-    });
-
     if(config.presenter_diagnostics()) {
         app.get('/crash', crash);
     }
--- src/services/template.js
+++ src/services/template.js
@@ -2,22 +2,39 @@ var async = require('async');
 var fs = require('fs');
 var globby = require('globby');
 var path = require('path');
+var logger = require('../server/logging').logger;
 var services = {
     nunjucks: require('./nunjucks'),
-    path: require('./path')
+    path: require('./path'),
 };
+var ResponseHelper = require('../helpers/response');
+var HttpErrorHelper = require('../helpers/http-error');
 
 var TemplateService = {
     render: function (templatePath, data) {
         var templateFile = this._findTemplate(templatePath);
+        var templateData = this._bootstrapContext(data);
+
         try {
-            return services.nunjucks.render(templateFile, data);
+            var output = services.nunjucks.render(templateFile, templateData);
+            ResponseHelper.send(output);
         }
         catch (e) {
-            console.log(e);
-            return e;
+            logger.error(e);
+            this.render('500');
         }
 
+    },
+    _bootstrapContext: function (content) {
+        return {
+            deconst: {
+                env: process.env,
+                content: content || {},
+                url: require('./url'),
+                request: require('../helpers/request'),
+                response: require('../helpers/response')
+            }
+        };
     },
     _findTemplate: function (templatePath) {
         templatePath = templatePath || 'index';
