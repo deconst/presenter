ccc86640dec85d4d55c646a504ffbea52ea567bb
Ash Wilson <smashwilson@gmail.com>
2015-10-20 16:15:50 -0400

    Log more error details.

--- src/helpers/context.js
+++ src/helpers/context.js
@@ -15,7 +15,7 @@ function Context (req, resp) {
   this.templateRenderDuration = null;
 }
 
-Context.prototype._summarize = function (statusCode, message) {
+Context.prototype._summarize = function (statusCode, message, err) {
   var summary = {
     statusCode: statusCode,
     message: message,
@@ -45,6 +45,11 @@ Context.prototype._summarize = function (statusCode, message) {
     summary.templateRenderDuration = this.templateRenderDuration;
   }
 
+  if (err) {
+    summary.errMessage = err.message;
+    summary.stack = err.stack;
+  }
+
   return summary;
 };
 
@@ -62,6 +67,7 @@ Context.prototype.send = function (body) {
 };
 
 Context.prototype.handleError = function (err) {
+  var original = err;
   logger.debug(err);
   var code = err.statusCode || 500;
   if (err.statusCode && err.statusCode.toString() === '404') {
@@ -77,7 +83,7 @@ Context.prototype.handleError = function (err) {
     this.response.status(code).send(responseBody);
 
     if (code >= 500) {
-      logger.warn(this._summarize(code, 'Internal error'));
+      logger.warn(this._summarize(code, 'Internal error', original));
     } else {
       logger.info(this._summarize(code, 'Request error'));
     }
