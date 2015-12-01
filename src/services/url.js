b5070fb292d18779fe5c812ff9065a629a703fc4
Ash Wilson <smashwilson@gmail.com>
2015-12-01 15:22:14 -0500

    Always include trailing slashes on presented URLs.

--- src/services/url.js
+++ src/services/url.js
@@ -2,16 +2,23 @@ var url = require('url');
 
 var SITE_DIRECTORY = '/';
 
+var withTrailingSlash = function (u) {
+  if (u[u.length - 1] === '/') {
+    return u;
+  }
+  return u + '/';
+};
+
 var UrlService = {
   getSiteUrl: function (context, path) {
     path = path || '';
     var siteUrl = context.protocol() + '://' + context.host();
 
-    return url.resolve(siteUrl + SITE_DIRECTORY, path);
+    return withTrailingSlash(url.resolve(siteUrl + SITE_DIRECTORY, path));
   },
   getSitePath: function (path) {
     path = path || '';
-    return url.resolve(SITE_DIRECTORY, path);
+    return withTrailingSlash(url.resolve(SITE_DIRECTORY, path));
   }
 };
 
