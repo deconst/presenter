2475f4b64a5d77303f66278fb49c404e01c4e266
Ash Wilson <smashwilson@gmail.com>
2015-12-01 14:39:10 -0500

    ContentService function to perform searches.

--- src/services/content/index.js
+++ src/services/content/index.js
@@ -104,6 +104,59 @@ var ContentService = {
       callback(null, JSON.parse(body));
     });
   },
+  getSearch: function (q, pageNumber, perPage, callback) {
+    var searchUrl = urljoin(config.content_service_url(), 'search');
+    var searchQuery = {
+      q: q,
+      pageNumber: pageNumber,
+      perPage: perPage
+    };
+    var reqStart = Date.now();
+
+    logger.debug('Content service request: performing search.', {
+      url: searchUrl,
+      query: searchQuery
+    });
+
+    request({ url: searchUrl, qs: searchQuery }, function (err, res, body) {
+      var reqDuration = Date.now() - reqStart;
+      if (err) return callback(err);
+
+      if (res.statusCode === 404) {
+        // Compatibility with older content services
+        return callback(null, { total: 0, results: [] });
+      }
+
+      if (res.statusCode >= 400) {
+        var messageBody;
+        try {
+          messageBody = JSON.parse(body);
+        } catch (e) {
+          messageBody = body || 'Empty response';
+        }
+
+        return callback({
+          statusCode: res.statusCode,
+          message: messageBody,
+          searchReqDuration: reqDuration
+        });
+      }
+
+      var doc = {};
+      try {
+        doc = JSON.parse(body);
+      } catch (e) {
+        return callback(e);
+      }
+
+      logger.debug('Content service request: search successful.', {
+        resultCount: doc.results.length,
+        searchReqDuration: reqDuration
+      });
+
+      callback(null, doc);
+    });
+  },
   getControlSHA: function (context, callback) {
     var shaUrl = urljoin(config.content_service_url(), 'control');
     var reqStart = Date.now();
