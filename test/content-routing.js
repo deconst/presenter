b634c570741e9a3b8d275206e09af2eec287e26a
Ash Wilson <smashwilson@gmail.com>
2015-12-07 14:21:57 -0500

    Unit tests for ContentRoutingService

---
+++ test/content-routing.js
@@ -0,0 +1,62 @@
+/* globals it describe beforeEach */
+// Unit tests for the ContentRoutingService.
+
+var before = require('./helpers/before');
+var config = require('../src/config');
+var expect = require('chai').expect;
+
+config.configure(before.settings);
+
+var ContentRoutingService = require('../src/services/content/routing');
+
+describe.only('ContentRoutingService', function () {
+  var context = {
+    host: function () {
+      return 'deconst.horse';
+    }
+  };
+
+  beforeEach(function () {
+    ContentRoutingService.setContentMap({
+      'deconst.horse': {
+        content: {
+          '/prefix/': 'prefix',
+          '/one/two/': 'two',
+          '/one/': 'one',
+          '/without-slash': 'noslash',
+          '/empty/': null
+        }
+      }
+    });
+  });
+
+  var shouldMap = function (presentedPath, contentID) {
+    return function () {
+      expect(ContentRoutingService.getContentId(context, presentedPath)).to.equal(contentID);
+    };
+  };
+
+  it('returns a sentinel for umapped content',
+    shouldMap('/notmapped/', ContentRoutingService.UNMAPPED));
+
+  it('maps by prefix not substring',
+    shouldMap('/nope/prefix/other/', ContentRoutingService.UNMAPPED));
+
+  it('maps a presented path to a content ID',
+    shouldMap('/prefix/', 'prefix'));
+
+  it('maps a presented path to a content ID by prefix',
+    shouldMap('/prefix/something-else', 'prefix/something-else'));
+
+  it('maps presented paths to the longest prefix',
+    shouldMap('/one/two/thingy', 'two/thingy'));
+
+  it('does not require a trailing slash on presented paths',
+    shouldMap('/without-slash/blah', 'noslash/blah'));
+
+  it('maps null routes to the empty envelope',
+    shouldMap('/empty/', ContentRoutingService.EMPTY_ENVELOPE));
+
+  it('unmaps everything beneath a null route',
+    shouldMap('/empty/anythingelse', ContentRoutingService.UNMAPPED));
+});
