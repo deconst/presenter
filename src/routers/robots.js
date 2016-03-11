18605aee24a35a38b346c466b97da70371b6fe12
Ash Wilson <smashwilson@gmail.com>
2016-03-11 15:47:44 -0500

    Hardcode robots in staging mode.

---
+++ src/routers/robots.js
@@ -0,0 +1,14 @@
+'use strict';
+// Hardcode the robots.txt for staging servers. For production servers, fall through to the normal
+// content flow.
+
+const config = require('../config');
+const content = require('./content');
+
+module.exports = function (req, res) {
+  if (!config.staging_mode()) {
+    return content(req, res);
+  }
+
+  res.send('User-agent: *\nDisallow: /\n');
+};
