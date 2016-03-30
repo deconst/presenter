de691b9feba6090e52f56eebd4bef2ec902247a0
Ash Wilson <smashwilson@gmail.com>
2016-03-30 15:41:23 -0400

    Unnecessary describe()

--- test/api-mapping.js
+++ test/api-mapping.js
@@ -10,7 +10,7 @@ const request = require('supertest');
 const server = require('../src/server');
 const ControlService = require('../src/services/control');
 
-describe('the mapping API', () => {
+describe('/_api/whereis', () => {
   beforeEach(before.reconfigure);
   beforeEach((done) => {
     ControlService.load((ok) => {
@@ -19,61 +19,59 @@ describe('the mapping API', () => {
     });
   });
 
-  describe('/_api/whereis', () => {
-    it('returns the domain and paths of a content ID', (done) => {
-      request(server.create())
-        .get('/_api/whereis/https%3A%2F%2Fgithub.com%2Fdeconst%2Fsubrepo%2Fsomepath')
-        .set('Accept', 'application/json')
-        .expect(200)
-        .expect({
-          mappings: [
-            {
-              domain: 'deconst.horse',
-              path: '/subrepo/somepath/',
-              baseContentID: 'https://github.com/deconst/subrepo/',
-              basePath: '/subrepo/'
-            }
-          ]
-        }, done);
-    });
+  it('returns the domain and paths of a content ID', (done) => {
+    request(server.create())
+      .get('/_api/whereis/https%3A%2F%2Fgithub.com%2Fdeconst%2Fsubrepo%2Fsomepath')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .expect({
+        mappings: [
+          {
+            domain: 'deconst.horse',
+            path: '/subrepo/somepath/',
+            baseContentID: 'https://github.com/deconst/subrepo/',
+            basePath: '/subrepo/'
+          }
+        ]
+      }, done);
+  });
 
-    it('returns multiple results for redundantly mapped content', (done) => {
-      request(server.create())
-        .get('/_api/whereis/https%3A%2F%2Fgithub.com%2Fdeconst%2Fredundant')
-        .set('Accept', 'application/json')
-        .expect(200)
-        .expect({
-          mappings: [
-            {
-              domain: 'deconst.horse',
-              path: '/redundant/',
-              baseContentID: 'https://github.com/deconst/redundant/',
-              basePath: '/redundant/'
-            },
-            {
-              domain: 'deconst.horse',
-              path: '/multimapped/',
-              baseContentID: 'https://github.com/deconst/redundant/',
-              basePath: '/multimapped/'
-            },
-            {
-              domain: 'deconst.dog',
-              path: '/elsewhere/',
-              baseContentID: 'https://github.com/deconst/redundant/',
-              basePath: '/elsewhere/'
-            }
-          ]
-        }, done);
-    });
+  it('returns multiple results for redundantly mapped content', (done) => {
+    request(server.create())
+      .get('/_api/whereis/https%3A%2F%2Fgithub.com%2Fdeconst%2Fredundant')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .expect({
+        mappings: [
+          {
+            domain: 'deconst.horse',
+            path: '/redundant/',
+            baseContentID: 'https://github.com/deconst/redundant/',
+            basePath: '/redundant/'
+          },
+          {
+            domain: 'deconst.horse',
+            path: '/multimapped/',
+            baseContentID: 'https://github.com/deconst/redundant/',
+            basePath: '/multimapped/'
+          },
+          {
+            domain: 'deconst.dog',
+            path: '/elsewhere/',
+            baseContentID: 'https://github.com/deconst/redundant/',
+            basePath: '/elsewhere/'
+          }
+        ]
+      }, done);
+  });
 
-    it('returns an empty collection for unknown content IDs', (done) => {
-      request(server.create())
-        .get('/_api/whereis/https%3A%2F%2Fgithub.com%2Fnope%2Fnope')
-        .set('Accept', 'application/json')
-        .expect(200)
-        .expect({
-          mappings: []
-        }, done);
-    });
+  it('returns an empty collection for unknown content IDs', (done) => {
+    request(server.create())
+      .get('/_api/whereis/https%3A%2F%2Fgithub.com%2Fnope%2Fnope')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .expect({
+        mappings: []
+      }, done);
   });
 });
